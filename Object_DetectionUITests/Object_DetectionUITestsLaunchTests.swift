221e111689a3557faeb6aa59edfbf1a834c43192
hobaoan <20520876@gm.uit.edu.vn>
2023-10-18 13:05:06 +0700

    Initial Commit

---
+++ Object_DetectionUITests/Object_DetectionUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Object_DetectionUITestsLaunchTests.swift
+//  Object_DetectionUITests
+//
+//  Created by Hồ Bảo An on 18/10/2023.
+//
+
+import XCTest
+
+final class Object_DetectionUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
